a5f90a884f779bb8f6628e53a3563c30bb839108
Fekete Mihai <feketemihai@nexterp.ro>
2024-09-05 16:40:27 +0300

    Update __manifest__.py

--- l10n_ro_demo_data/__manifest__.py
+++ l10n_ro_demo_data/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Demo Data Module",
-    "version": "14.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "General",
     "author": "NextERP Romania SRL",
     "website": "https://github.com/OCA/l10n-romania",
